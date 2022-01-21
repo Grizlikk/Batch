991c1629bc851a5783a319e111b0492a2674fba0
Grizlik :D <96445475+Grizlikk@users.noreply.github.com>
2022-01-21 09:42:53 +0100

    Update

--- Random.bat
+++ Random.bat
@@ -1,11 +1,16 @@
 @echo off
+title Random Alpha 1.1
 color 6
 echo Tento program otestuje, jestli prikaz random dovede opravdu vygenerovat vsechny cisla od 0 az do 32 768
 echo.
 pause
 
 cls
-set /a i = 0
+set /p i=Zvolte hodnotu, od ktere ma zacit testovani: 
+if %i% lss 0 echo Neplatna hodnota! & pause & exit
+if %i% gtr 32769 echo Neplatna hodnota! & pause & exit
+cls
+
 :a
 if %random% == %i% (echo %i%
     set /a i = %i% + 1)
